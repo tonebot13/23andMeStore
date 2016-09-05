811426ca98eef0b7eb3e182840b1393164ce9184
tonebot13 <trmellors@gmail.com>
2016-09-05 15:32:44 -0700

    renamed file - wip

---
+++ 23_store_kits_wait.py
@@ -0,0 +1,96 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# wip
+class TestStoreCart(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_store_cart_transaction(self):
+        driver = self.driver
+
+        # store landing page
+        driver.get("https://store.23andme.com/en-us/cart/")
+        self.assertEqual(driver.current_url,
+                         "https://store.23andme.com/en-us/cart/")
+        print(driver.current_url)
+
+        # add first kit
+        driver.find_element_by_xpath("//*[.='Add a kit.']").click()
+
+        # add addtional kits
+        WebDriverWait(driver, 12).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".add-remove-kits .js-add-kit")))
+        for x in range(0, 4):
+            driver.find_element_by_css_selector(
+                ".add-remove-kits .js-add-kit").click()
+        name_input_list = driver.find_elements_by_css_selector(
+            ".kit-list .js-kit-name")
+
+        # give kits unique name
+        count = 0
+        for name_input in name_input_list:
+            name_input.send_keys("Test%s" % count)
+            count += 1
+        time.sleep(5)
+        driver.find_element_by_xpath(
+            "//input[@class='submit button-continue']").click()
+
+        # Address form
+        time.sleep(5)
+        self.assertEqual(driver.current_url,
+                         "https://store.23andme.com/en-us/shipping/")
+        print(driver.current_url)
+        driver.find_element_by_xpath(
+            "//input[@id='id_first_name']").send_keys("Sandy")
+        driver.find_element_by_xpath(
+            "//input[@id='id_last_name']").send_keys("Thomson")
+        driver.find_element_by_xpath(
+            "//input[@id='id_company']").send_keys("self")
+        driver.find_element_by_xpath(
+            "//input[@id='id_address']").send_keys("6573 Northridge Dr.")
+        driver.find_element_by_xpath(
+            "//input[@id='id_address2']").send_keys("House")
+        driver.find_element_by_xpath(
+            "//input[@id='id_city']").send_keys("San Jose")
+        driver.find_element_by_xpath(
+            "//select[@id='id_state']").send_keys("California")
+        driver.find_element_by_xpath(
+            "//input[@id='id_postal_code']").send_keys("95120")
+        driver.find_element_by_xpath("//select[@id='id_country']")
+        driver.find_element_by_xpath(
+            "//input[@id='id_email']").send_keys("sthomson@netscape.net")
+        driver.find_element_by_xpath(
+            "//input[@id='id_int_phone']").send_keys("1 408-268-0001")
+        driver.find_element_by_xpath("//input[@id='id_add_gift']").click()
+        driver.find_element_by_xpath("//textarea[@id='id_gift_message']").send_keys(
+            "gift message gift message gift message gift message gift message gift")                  
+        driver.find_element_by_xpath(
+            "//input[@class='submit button-continue']").click()
+
+        # verify address page.
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@value='ship to verified address']")))
+        self.assertEqual(driver.current_url,
+                         "https://store.23andme.com/en-us/verifyaddress/")
+        print(driver.current_url)
+        driver.find_element_by_xpath(
+            "//input[@value='ship to verified address']").click()
+        
+        # lands on payment page.
+        self.assertEqual(driver.current_url,
+                         "https://store.23andme.com/en-us/payment/")
+        print(driver.current_url)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
